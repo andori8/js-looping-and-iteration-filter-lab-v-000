36170f8b5bf5033437bc857501423304acd1da6a
Ryan Do <falcsmike7@gmail.com>
2017-11-03 00:17:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching (drivers, name) {
-  return drivers.filter(function (element) { return element === name(0).toUpperCase(); });
+  return drivers.filter(function (element) { return element === name[0].toUpperCase(); });
 }
