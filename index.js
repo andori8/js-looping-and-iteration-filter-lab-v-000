36e972d7b476abe790a36b7d7d00b4c13bf9e2f0
Ryan Do <falcsmike7@gmail.com>
2017-11-03 00:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
-function findMatching (drivers, name) {
-  return drivers.filter(function (element) { return element === name; });
+function findMatching (collection, string) {
+  return collection.filter(function (element) { return element.toLowerCase() === string.toLowerCase(); });
 }
